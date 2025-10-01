e1fc1d2b8eb918952c7f82bf89ac7f265d69b22b
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-10-01 12:37:12 +0530

    Create CollectionOperationSafe.java
    for preventing this pitfalll use 
    CopyOnWriteArrayList<Integer> list= new CopyOnWriteArrayList<>();
     inplace of
    List<Integer> list= new ArrayList<>();

---
+++ Threads/pitfalls/CollectionOperationSafe.java
@@ -0,0 +1,34 @@
+package com.test.pitfalls;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CollectionOperationSafe {
+	public static void main(String[] args) throws InterruptedException {
+	//List<Integer> list= new ArrayList<>();
+	CopyOnWriteArrayList<Integer> list= new CopyOnWriteArrayList<>();
+	Thread t1= new Thread(() -> {
+		for(int i=0;i<100;i++) {
+			list.add(i);		}
+	});
+	
+	Thread t2= new Thread(() -> {
+		for(int i=1000;i<2000;i++) {
+			list.add(i);		}
+	});
+	t1.start();
+	t2.start();
+	
+	t1.join();
+	t2.join();
+	
+	
+	System.out.println("Total Size: "+ list.size());
+	
+	}
+
+}
+
+//code demonstrates a threading pitfall known as a race condition due to unsafe access
+// to a non-thread-safe collection — in this case, ArrayList.
