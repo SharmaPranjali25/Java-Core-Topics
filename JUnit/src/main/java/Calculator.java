ccf0c6aed59d7cbebaf47675f8e8da08d233036d
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-10-03 11:19:16 +0530

    Create Calculator.java

---
+++ JUnit/src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.List;
+
+public class Calculator {
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	public int mul(int x, int y) {
+		return x*y;
+	}
+	public int nameCount(List<String> names) {
+		return names.size();
+	}
+	public String greetUsers(String msg) {
+		return msg;
+	}
+
+}
